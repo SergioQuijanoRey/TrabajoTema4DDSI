cacee0c793d2d2fdc8ce42adf684df94e655c4c0
Sergio Quijano Rey <sergiquijano@gmail.com>
2021-01-17 17:22:33 +0100

    feat: añado una estructura de codigo basica para lanzar unos cuantos comandos

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+class MongoDatabase:
+    """Clase quwe representa la base de datos de MongoDB"""
+
+    def __init__(self):
+        """Inicializa la clase"""
+        self.database_ip =  "172.20.0.2"
+        self.client = None
+        self.db = None
+
+        self.connect_to_database()
+
+    def connect_to_database(self):
+        """Realiza la conexion con la base de datos"""
+        self.client = MongoClient(self.database_ip)
+        self.db = self.client.admin
+
+    def execute_command(self, command: str):
+        """Ejecuta un comando dado y devuelve los resultados"""
+        return self.db.command(command)
+
+    def server_status(self):
+        """Muestra el status del servidor"""
+        return self.execute_command("serverStatus")
+
+
+
+if __name__ == "__main__":
+    mongo = MongoDatabase()
+    pprint(mongo.server_status())
+
