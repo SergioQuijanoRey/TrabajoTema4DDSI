47cee1a2adadd199a6adc443e4ec7ab30491e70e
Juanjo <jh16357@gmail.com>
2021-01-17 22:13:10 +0100

    añado estructura de datos de asistente y dos consultas, una para los que tienen covid y otra en base al dni

--- src/main.py
+++ src/main.py
@@ -72,6 +72,78 @@ class MongoDatabase:
         for result in query_result:
             print(result)
 
+    def insert_assistant_data(self):
+        """
+        Insertamos datos en una coleccion 'Asistentes'
+        """
+        # Datos ficticios sobre los clientes
+        assistant_name = [
+            "Pilar Aranda 1",
+            "Pilar Aranda 2",
+            "Pilar Aranda 3",
+            "Pilar Aranda 4",
+            "Pilar Aranda 5",
+            "Pilar Aranda 6",
+            "Pilar Aranda 7",
+            "Pilar Aranda 8",
+            "Pilar Aranda 9",
+            "Pilar Aranda 10"
+        ]
+
+        assistant_id = [
+            "0000000k",
+            "0000001k",
+            "0000002k",
+            "0000003k",
+            "0000004k",
+            "0000005k",
+            "0000006k",
+            "0000007k",
+            "0000008k",
+            "0000009k"
+        ]
+
+        assistant_bank_account = [
+            "123456780jjx",
+            "123456781jjx",
+            "123456782jjx",
+            "123456783jjx",
+            "123456784jjx",
+            "123456785jjx",
+            "123456786jjx",
+            "123456787jjx",
+            "123456788jjx",
+            "123456789jjx"
+        ]
+
+
+        # Insertamos los datos
+        for index in range(len(assistant_name)):
+
+            if index%2 == 0:
+                contagiado = True
+            else:
+                contagiado = False
+
+            assistant_data = {
+                'name': assistant_name[index],
+                'dni': assistant_id[index],
+                'bank_account': assistant_bank_account[index],
+                'covid': contagiado
+            }
+
+            result = self.db.assistant.insert_one(assistant_data)
+
+
+    def select_assistant_given_dni(self, input_dni: str):
+        """Selecciona los clientes dado el dni """
+
+        return self.db.assistant.find({'dni': input_dni})
+    
+    def select_assistant_with_covid(self):
+        """Selecciona los clientes dado el dni """
+
+        return self.db.assistant.find({'covid': True})
 
 if __name__ == "__main__":
     mongo = MongoDatabase()
@@ -89,3 +161,16 @@ if __name__ == "__main__":
     print("Los clientes de Santander son:")
     print("=" * 80)
     mongo.print_query_result(mongo.select_client_given_city("Santander"))
+
+    print("-" * 100)
+
+    # Hacemos una consulta de los asistentes
+    mongo.insert_assistant_data()
+
+    dni = "0000003k"
+    print(f"El asistente con dni {dni} es: " )
+    mongo.print_query_result(mongo.select_assistant_given_dni(dni))
+
+    print("="*80)
+    print("Los asistentes que tienen covid son ")
+    mongo.print_query_result(mongo.select_assistant_with_covid())
